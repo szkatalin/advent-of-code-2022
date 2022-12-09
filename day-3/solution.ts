d23a895c2fc638a09ea585208662b2a464436f98
szkatalin <szkatalin@outlook.com>
2022-12-09 08:50:29 +0100

    solve day 3/a

--- day-3/solution.ts
+++ day-3/solution.ts
@@ -1,5 +1,7 @@
 import { promises as fs } from "fs";
 
+const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 export const readInputFile = async (): Promise<string[]> =>
   await fs.readFile("day-3/input.txt", "utf8").then((data) => {
     return data.split(/\n/g).map((v) => v);
@@ -7,7 +9,29 @@ export const readInputFile = async (): Promise<string[]> =>
 
 const main = () => {
   readInputFile().then((data) => {
-    console.log(data);
+    const value = data
+      .map((line) => {
+        const alphabets = line.split("");
+        const [first, second] = [
+          alphabets.slice(0, alphabets.length / 2),
+          alphabets.slice(alphabets.length / 2),
+        ];
+        let alphabet;
+        for (let i = 0; i < first.length; i++) {
+          let exit = false;
+          for (let j = 0; j < second.length && !exit; j++) {
+            if (first[i] === second[j]) {
+              alphabet = first[i];
+              exit=true;
+            }
+          }
+        }
+        return alphabet;
+      })
+      .map((a) => abc.split("").findIndex((v) => v === a) + 1)
+      .reduce((a, c) => a + c, 0);
+
+    console.log(value);
   });
 };
 
